1087ea09574c2765b389c29199dfe7eedb50fd47
Ganesh Gopalkrishna Hegde <31041477+adhvaitaganesh@users.noreply.github.com>
2023-12-27 13:27:53 +0100

    Update TokenDetail.tsx

--- app/[contractAddress]/[tokenId]/[chainId]/TokenDetail.tsx
+++ app/[contractAddress]/[tokenId]/[chainId]/TokenDetail.tsx
@@ -1,5 +1,5 @@
 import { Variants, motion } from "framer-motion";
-import { GalverseLogo, TokenboundLogo, MrkdLogo } from "@/components/icon";
+import { GalverseLogo, TokenboundLogo, KeziLogo } from "@/components/icon";
 import { Panel } from "./Panel";
 import { TbaOwnedNft } from "@/lib/types";
 
@@ -40,7 +40,7 @@ type LogoType = {
 };
 
 const Logo: LogoType = {
-  DEFAULT: MrkdLogo,
+  DEFAULT: keziLogo,
   GALVERSE: GalverseLogo,
 };
 
